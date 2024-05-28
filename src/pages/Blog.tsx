c04f91ec75f8530d5922149f1d57b5be4db502cb
mintech-dot <mintech@protonmail.com>
2024-05-28 10:12:03 +0100

    refactor: Improve code readability in pages folder

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,47 +1,15 @@
 import Post from "../components/ui/PostCard";
 
-type PostProps = {
-  title: string;
-  date: string;
-  categories: string;
-  description: string;
-};
-const postDetails: PostProps[] = [
-  {
-    title: "Making a design system from scratch",
-    date: "12 Feb 2020",
-    categories: "Design, Pattern",
-    description: `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-                    Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`,
-  },
-  {
-    title: "Creating pixel perfect icons in Figma",
-    date: "12 Feb 2020",
-    categories: "Design, Pattern",
-    description: `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-                    Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`,
-  },
-  {
-    title: "Making a design system from scratch",
-    date: "12 Feb 2020",
-    categories: "Design, Pattern",
-    description: `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-                    Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`,
-  },
-  {
-    title: "Creating pixel perfect icons in Figma",
-    date: "12 Feb 2020",
-    categories: "Design, Pattern",
-    description: `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-                    Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`,
-  },
-];
+import config from "../config/posts.json";
+// Define a type for the Post props
 
 const Blog = () => {
+  const { post } = config;
+
   return (
     <section className=" py-4 lg:px-[120px] sm:px-[45px]">
       <div className="grid grid-cols-1 gap-y-4 mx-4 md:mx-0">
-        {postDetails.map((post, index) => (
+        {post.map((post, index) => (
           <>
             <Post
               key={index}
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,43 +1,8 @@
 import FeatureItem from "../components/ui/FeatureItem";
-import feature1 from "../assets/feature1.png";
-import feature2 from "../assets/feature2.png";
-import feature3 from "../assets/feature3.png";
-interface Feature {
-  imgSrc: string;
-  title: string;
-  year: string;
-  category: string;
-  description: string;
-}
-
-const features: Feature[] = [
-  {
-    imgSrc: feature1,
-    title: "Designing Dashboards",
-    year: "2020",
-    category: "Dashboard",
-    description:
-      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-  },
-  {
-    imgSrc: feature2,
-    title: "Vibrant Portraits of 2020",
-    year: "2020",
-    category: "Illustration",
-    description:
-      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-  },
-  {
-    imgSrc: feature3,
-    title: "36 Days of Malayalam type",
-    year: "2020",
-    category: "Typography",
-    description:
-      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-  },
-];
+import config from "../config/works.json";
 
 const Works = () => {
+  const { features } = config;
   return (
     <section className="py-4 lg:px-[120px] sm:px-[45px]">
       <h1 className="text-[44px] text-dark font-black mx-4 md:mx-0">Works</h1>
